46d14720cb98bf6b1c64803b70b89d7d06a075b3
borisey <slamsnv@gmail.com>
2024-11-26 18:00:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, World!\n');
+});
+module.exports = app;
